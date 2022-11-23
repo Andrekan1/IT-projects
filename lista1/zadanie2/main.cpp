4c3581881e55f2ee14be36c4b8224bf360001b20
Andrekan1 <58864906+Andrekan1@users.noreply.github.com>
2022-11-23 21:12:11 +0100

    Add files via upload

--- lista1/zadanie2/main.cpp
+++ lista1/zadanie2/main.cpp
@@ -4,25 +4,65 @@
 using namespace std;
 
 struct Vector {
-	static double Length(double tab[2][2]) {
-		double temptab[2];
-		for(short i = 0; i <= 1; i++)
-			temptab[i] = pow(tab[1][i]-tab[0][i],2);
-		return sqrt(temptab[0]+temptab[1]);
-	}
+	public:
 
-	static double Dot(double tab[2][2]) {
-		double temptab[2];
-		for(short i = 0; i <= 1; i++)
-			temptab[i] = tab[i][0]*tab[i][1];
-		return temptab[0]+temptab[1];
-	}
+		static auto Length(double tab[2][2]) {
+			double temptab[2];
+			for(short i = 0; i <= 1; i++)
+				temptab[i] = pow(tab[1][i]-tab[0][i],2);
 
-	static bool Equal(double tab[2]) {
-		if(tab[0] == tab[1])
-			return true;
-		return false;
-	}
+			return sqrt(temptab[0]+temptab[1]);
+		}
+
+		static auto Dot(double tab[2][2]) {
+			double temptab[2];
+			for(short i = 0; i <= 1; i++)
+				temptab[i] = tab[0][i]*tab[1][i];
+
+			return temptab[0]+temptab[1];
+		}
+
+		static auto Equal(const double tab[2]) {
+			if(tab[0] == tab[1])
+				return true;
+
+			return false;
+		}
+
+		static auto AAvg(double tab[2][2]) {
+			double temptab[2] = {0,0};
+			for(short i = 0; i <= 1; i++)
+				temptab[i] = (tab[0][i] + tab[1][i]) / 2;
+
+			return pair(temptab[0],temptab[1]);
+		}
+
+		static auto GAvg(double tab[2][2]) {
+			double temptab[2] = {1,1};
+			for(short i = 0; i <= 1; i++)
+				temptab[i] = sqrt(tab[0][i] * tab[1][i]);
+
+			return pair(temptab[0],temptab[1]);
+		}
+
+		static auto Median(double tab[2][2]) {
+			double tab1d[4];
+			for(short i = 0; i <= 1; i++)
+				for(short j = 0; j <= 1; j++)
+					tab1d[i==0 ? j : i+j+1] = tab[i][j];
+
+			for(short i = 0; i < 4; i++) {
+				for(short j = 0; j < 3; j++) {
+					if(tab1d[j] > tab1d[j+1]) {
+						double temp = tab1d[j + 1];
+						tab1d[j + 1] = tab1d[j];
+						tab1d[j] = temp;
+					}
+				}
+			}
+
+			return (tab1d[1]+tab1d[2])/2;
+		}
 };
 
 double tableMaxElement(double tab[2][2]) {
@@ -42,20 +82,28 @@ int main() {
             cin >> j;
 
 	double len = Vector::Length(tab);
-	cout << "Dlugosc wektora wynosi: " << len << endl << endl;
-
+	cout << "\nDlugosc wektora wynosi: " << len << endl;
 
 	tab[2][2] = {};
 	double vectors[2];
 	for (short k = 0; k < 2; k++) {
-		cout << "Podaj wspolrzedne dla " << (k == 0 ? "pierwszego" : "drugiego") << " wektrora" << endl;
+		/*cout << "Podaj wspolrzedne dla " << (k == 0 ? "pierwszego" : "drugiego") << " wektrora" << endl;
 		for(auto &i: tab) {
 			for(double &j: i) {
 				cin >> j;
 			}
-		}
+		}*/
 		vectors[k] = Vector::Length(tab);
 	}
+
+	pair<double,double> aavg = Vector::AAvg(tab);
+	cout << "Srednia arytmetyczna wektoru to: [" << aavg.first << ", " << aavg.second << "]" << endl;
+
+	pair<double,double> gavg = Vector::GAvg(tab);
+	cout << "Srednia geometryczna wektoru to: [" << gavg.first << ", " << gavg.second << "]" << endl;
+
+	double median = Vector::Median(tab);
+	cout << "Mediana wektoru to: " << median << endl;
 
 	if(Vector::Equal(vectors)) {
 		double dot = Vector::Dot(tab);
